ca035279c868af473588964171d30b8636fa6a3a
Xiang <xdong57@emory.edu>
2020-02-28 14:30:51 -0500

    update regression model

--- transformers/__init__.py
+++ transformers/__init__.py
@@ -157,6 +157,7 @@ if is_torch_available():
         BertForMultipleChoice,
         BertForTokenClassification,
         BertForQuestionAnswering,
+        BertForOffensiveClassification,
         load_tf_weights_in_bert,
         BERT_PRETRAINED_MODEL_ARCHIVE_MAP,
     )
